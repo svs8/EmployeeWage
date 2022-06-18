ff34f1eb7149325e79ec2b270296b371ff078210
svs8 <surakshithvittalshetty@gmail.com>
2022-06-18 14:08:23 +0530

    Added:Calculating Employee working Hours and the wage for an period of 20 days

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,9 +1,10 @@
-//UC3
+//UC4
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 20;
 function getWorkingHours(employeeCheck){
     switch (employeeCheck) {
         case IS_PART_TIME:
@@ -14,8 +15,10 @@ function getWorkingHours(employeeCheck){
             return 0;
     }
 }
-let employeeCheck = Math.floor(Math.random() * 10) % 3;
-let employeeHours = getWorkingHours(employeeCheck);
-let empWage = employeeHours * WAGE_PER_HOUR;
-console.log(" Hours = "+employeeHours+" Employee Wage = " + empWage);
-
+let totalEmpHrs = 0;
+for(let day = 0; day<NUM_OF_WORKING_DAYS; day++){
+   let empCheck = Math.floor(Math.random() * 10) % 3;
+    totalEmpHrs += getWorkingHours(empCheck);
+}
+let empWage = totalEmpHrs * WAGE_PER_HOUR;
+console.log(" Total Hours = "+totalEmpHrs+" Employee Wage = " + empWage);
