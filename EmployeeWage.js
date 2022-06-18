fc5924b5d31ce950865eabfed230b4f068a9e881
svs8 <surakshithvittalshetty@gmail.com>
2022-06-18 14:15:16 +0530

    Added:Calculating Employee wage for an period of 20 days or 160 Hours using While loop

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,10 +1,11 @@
-//UC4
+//UC5
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 const NUM_OF_WORKING_DAYS = 20;
+const MAX_HOURS_IN_MONTH = 160;
 function getWorkingHours(employeeCheck){
     switch (employeeCheck) {
         case IS_PART_TIME:
@@ -16,9 +17,11 @@ function getWorkingHours(employeeCheck){
     }
 }
 let totalEmpHrs = 0;
-for(let day = 0; day<NUM_OF_WORKING_DAYS; day++){
-   let empCheck = Math.floor(Math.random() * 10) % 3;
-    totalEmpHrs += getWorkingHours(empCheck);
+let totalWorkingDays = 0;
+while(totalEmpHrs <= MAX_HOURS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+    totalWorkingDays++;
+    employeeCheck = Math.floor(Math.random() * 10) % 3;
+    totalEmpHrs += getWorkingHours(employeeCheck);
 }
 let empWage = totalEmpHrs * WAGE_PER_HOUR;
-console.log(" Total Hours = "+totalEmpHrs+" Employee Wage = " + empWage);
+console.log("Total Days : "+totalWorkingDays+" Total Hours : "+totalEmpHrs+" Total Employee Wage : " + empWage);
