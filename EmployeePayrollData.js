e156f07277f966cff997d5ca11175d1d6107a82c
svs8 <surakshithvittalshetty@gmail.com>
2022-06-19 23:50:38 +0530

    Added:Using Class EmployeePayrollData

---
+++ EmployeePayrollData.js
@@ -0,0 +1,70 @@
+class EmployeePayrollData {
+
+    id;
+    salary;
+    gender;
+    startDate;
+
+    constructor(...params) {
+        this.id = params[0];
+        this.name = params[1];
+        this.salary = params[2];
+        this.gender = params[3];
+        this.startDate = params[4];
+    }
+
+    get id() {
+        return this._id;
+    }
+
+    set id(id) {
+        this._id = id;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(name) {
+        this._name = name;
+    }
+
+    get salary() {
+        return this._salary;
+    }
+
+    set salary(salary) {
+        this._salary = salary;
+    }
+
+    get gender() {
+        return this._gender;
+    }
+
+    set gender(gender) {
+        this._gender = gender;
+    }
+
+    get startDate() {
+        return this._startDate;
+    }
+
+    set startDate(startDate) {
+        this._startDate = startDate;
+    }
+
+    toString() {
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        const employeeDate = this.startDate == undefined ? "undefined" : this.startDate.toLocaleDateString("en-us", options);
+        return "Id = " + this.id + ", Name = " + this.name + ", Gender = " + this.gender + ", Salary = " + this.salary + ", Start Date = " + employeeDate;
+    }
+}
+
+let employeePayrollData = new EmployeePayrollData(1, "Mark", 30000);
+console.log(employeePayrollData.toString());
+employeePayrollData.id = 0;
+employeePayrollData.name = "Jeff";
+console.log(employeePayrollData.toString());
+
+let terrisaPayrollData = new EmployeePayrollData(1, "Terrisa", 30000, "F", new Date());
+console.log(terrisaPayrollData.toString());
